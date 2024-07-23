96b4feca07e74a8c405ac383861c117fbacc9014
Gayatri Gunjlor <gayatrigunjalor23@navgurukul.org>
2024-07-23 22:30:26 +0530

    addingfile

---
+++ CALCUJS.js
@@ -0,0 +1,19 @@
+function display(b){
+    document.getElementById("input").value+=b;
+    
+    }
+    
+    function calculation(){
+    let y =document.getElementById("input").value;
+    document.getElementById("input").value=eval(y);
+    
+
+    }
+    
+    function hii(){
+        document.getElementById("input").value="";
+    
+    
+    
+    
+    }
